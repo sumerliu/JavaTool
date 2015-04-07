71efb6ce00e0fe18b525d09de4373c18be8d9ccc
Sumer <livialiu1234@outlook.com>
2015-04-08 00:07:15 +0800

    更新数字接口注释

--- src/com/sumer/util/ToolForNum.java
+++ src/com/sumer/util/ToolForNum.java
@@ -6,9 +6,10 @@ public interface ToolForNum {
 	 * 
 	 * @Description 该方法用以获取4位随机数字符串
 	 * @author liutj
+	 * @param digit 对输出随机数的位数进行限定
 	 * @return 
-	 * String
-	 * 2015年4月7日
+	 * String 返回一个随机数字符串
+	 * 2015年4月8日
 	 */
 	String getRandom4Num(int digit);
 
